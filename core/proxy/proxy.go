0514538f31fad48bb344c3f8b07e889de809266d
Justine Paralejas <paralejasjustine15@gmail.com>
2025-10-04 15:53:34 +0800

    debug

--- core/proxy/proxy.go
+++ core/proxy/proxy.go
@@ -5,111 +5,125 @@ import (
 	"context"
 	"io"
 	"net"
+	"sync"
 	"time"
 )
+
+var bufferPool = sync.Pool{
+	New: func() any {
+		buf := make([]byte, 32*1024)
+		return &buf
+	},
+}
 
 // Stream handles bidirectional stream between src and dst.
 func Stream(src, dst io.ReadWriter) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	errch := make(chan error, 2)
-
+	errch := make(chan error, 1)
 	// Copy src -> dst
 	go func() {
 		err := CopyWithContext(ctx, dst, src)
-		errch <- err
+		select {
+		case errch <- err:
+		default:
+		}
 	}()
-
 	// Copy dst -> src
 	go func() {
 		err := CopyWithContext(ctx, src, dst)
-		errch <- err
+		select {
+		case errch <- err:
+		default:
+		}
 	}()
-
 	err := <-errch
-	cancel()
 	closeConnection(src)
 	closeConnection(dst)
-
-	<-errch
-
 	return err
 }
 
 // StreamWithContext is Stream with context.
 func StreamWithContext(ctx context.Context, src, dst io.ReadWriter) error {
-	errch := make(chan error, 2)
+	errch := make(chan error, 1)
 	localCtx, cancel := context.WithCancel(ctx)
 	defer cancel()
-
 	// Copy src -> dst
 	go func() {
 		err := CopyWithContext(localCtx, dst, src)
-		errch <- err
+		select {
+		case errch <- err:
+		default:
+		}
 	}()
-
 	// Copy dst -> src
 	go func() {
 		err := CopyWithContext(localCtx, src, dst)
-		errch <- err
+		select {
+		case errch <- err:
+		default:
+		}
 	}()
-
-	var firstErr error
-
 	select {
-	case firstErr = <-errch:
-		cancel()
+	case <-errch:
 	case <-ctx.Done():
-		cancel()
-		firstErr = ctx.Err()
 	}
-
+	cancel()
 	closeConnection(src)
 	closeConnection(dst)
-
-	<-errch
-	if firstErr == nil {
-		firstErr = <-errch
-	} else {
-		<-errch
-	}
-
-	return firstErr
+	return <-errch
 }
 
 // CopyWithContext performs io.Copy with context cancellation.
 func CopyWithContext(ctx context.Context, dst, src io.ReadWriter) error {
+	bufPtr := bufferPool.Get().(*[]byte)
+	defer bufferPool.Put(bufPtr)
+	buf := *bufPtr
+
 	if conn, ok := src.(net.Conn); ok {
 		go func() {
 			<-ctx.Done()
 			conn.Close()
 		}()
 	}
-
-	if conn, ok := src.(net.Conn); ok {
-		if deadline, ok := ctx.Deadline(); ok {
-			conn.SetReadDeadline(deadline)
-		} else {
-			conn.SetReadDeadline(time.Now().Add(30 * time.Second))
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
 		}
-	}
-
-	if conn, ok := dst.(net.Conn); ok {
-		if deadline, ok := ctx.Deadline(); ok {
-			conn.SetWriteDeadline(deadline)
-		} else {
-			conn.SetWriteDeadline(time.Now().Add(30 * time.Second))
+		if conn, ok := src.(net.Conn); ok {
+			if deadline, ok := ctx.Deadline(); ok {
+				conn.SetReadDeadline(deadline)
+			} else {
+				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+			}
 		}
-	}
-
-	_, err := io.Copy(dst, src)
-
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-		return err
+		n, readErr := src.Read(buf)
+		if n > 0 {
+			if conn, ok := dst.(net.Conn); ok {
+				if deadline, ok := ctx.Deadline(); ok {
+					conn.SetWriteDeadline(deadline)
+				} else {
+					conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
+				}
+			}
+			_, writeErr := dst.Write(buf[:n])
+			if writeErr != nil {
+				return writeErr
+			}
+		}
+		if readErr != nil {
+			if netErr, ok := readErr.(net.Error); ok && netErr.Timeout() {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				default:
+					continue
+				}
+			}
+			return readErr
+		}
 	}
 }
 
