64e47675972a7b392c2584e0b6b3abab29098eb4
Justine Paralejas <paralejasjustine15@gmail.com>
2026-01-11 18:10:09 +0800

    fix: send err

--- core/server/server.go
+++ core/server/server.go
@@ -260,7 +260,7 @@ func (s *Server) handleRequest(ctx context.Context, stream net.Conn, session *ya
 
 	req, err := proto.DeserializeRequest(*bufPtr)
 	if err != nil {
-		sendErr := s.sendErr(stream, "failed to deserialize request")
+		sendErr := s.sendErr(stream, fmt.Sprintf("failed to deserialize request: %w", err))
 		if sendErr != nil {
 			return errors.Join(err, sendErr)
 		}
